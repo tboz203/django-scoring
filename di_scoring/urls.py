9766ce861022e2f42d42ced156966099f18c2397
tboz203 <tboz203@gmail.com>
2015-03-16 20:25:42 -0500

    Team List View and Team Detail View created
    got the beginnings of the templates down, added a project `base.css` file,
    created and connected team list view and team detail view views, urls, and
    templates.

--- di_scoring/urls.py
+++ di_scoring/urls.py
@@ -3,9 +3,6 @@ from di_scoring import views
 
 urlpatterns = patterns('',
     url(r'^$', views.indexView, name='index'),
-    # url(r'login/$', 'django.contrib.auth.views.login',
-    #     {'template_name': 'admin/login.html'},
-    #     name='login',
-    # ),
-    # url(r'logout/', 'django.contrib.auth.views.logout', name='logout'),
+    url(r'^teams/$', views.TeamListView.as_view(), name='team_list'),
+    url(r'^teams/(?P<slug>[\w\ ]+)/$', views.TeamDetailView.as_view(), name='team_detail'),
 )
--- di_scoring/views.py
+++ di_scoring/views.py
@@ -1,4 +1,14 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from di_scoring import models
+
 
 def indexView(request):
     return render(request, 'di_scoring/index.html')
+
+class TeamListView(ListView):
+    model = models.Team
+
+class TeamDetailView(DetailView):
+    model = models.Team
+    slug_field = 'name'
